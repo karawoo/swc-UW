d024d203998bb915b87a29b9df5fb5a31db9dd73
Kara Woo <woo.kara@gmail.com>
2014-03-18 12:19:55 -0700

    Change abline color to red.

--- src/toyline.R
+++ src/toyline.R
@@ -6,6 +6,6 @@ sigSq <- 0.5
 x <- runif(40)
 y <- a + b * x + rnorm(40, sd = sqrt(sigSq))
 plot(x, y)
-abline(a, b, col = "green")
+abline(a, b, col = "red")
 dev.print(png, "toylinePlot.png", width = 480)
 dev.print(pdf, "toylinePlot.pdf")
